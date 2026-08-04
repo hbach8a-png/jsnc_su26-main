790d586dd2599e49daaf39821585e678adffd8ae
Bach Hoang <hbach8a@gmail.com>
2026-08-04 14:37:41 +0700

    bach

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("HelloWord");
+//$ PHP
+// Khai bao bien
+// let var
+//let const
+
+//let
+
+//var ngay xua
+//
+let name = "hoadv";
+const age = 35;
+
+money = 50
+const yearCong =2004;
+
+const yearBach=2006;
+let age = 20;
